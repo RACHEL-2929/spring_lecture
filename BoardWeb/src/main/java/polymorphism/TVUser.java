81727a6b8181c6da1f8a883366e4e005899d4b85
RACHEL-2929 <sts06154@naver.com>
2024-07-08 17:10:18 +0900

    update

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -11,13 +11,14 @@ public class TVUser {
 	public static void main(String[] args) {
 
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
-		TV tv1 = (TV) factory.getBean("tv");// bean에서 등록되어 있는 id를 통해서 가지고 옴
-		TV tv2 = (TV) factory.getBean("tv");// 기본적으로 싱글톤 패턴이기 때문에 하나의 객체만 메모리에 생긴다
-												//만약 여러개의 객체를 생기게 하고 싶다면 bean 태그에서 scope 속성을 prototype으로 주면 된다.
-		TV tv3 = (TV) factory.getBean("tv");
-		/*
-		 * tv.powerOn(); tv.volumeUp(); tv.volumeDown(); tv.powerOff();
-		 */
+		TV tv = (TV) factory.getBean("tv");
+		// bean에서 등록되어 있는 id를 통해서 가지고 옴
+		// TV tv2 = (TV) factory.getBean("tv");
+		// 기본적으로 싱글톤 패턴이기 때문에 하나의 객체만 메모리에 생긴다 
+		// 만약 여러개의 객체를 생기게 하고 싶다면 bean 태그에서 scope 속성을 prototype으로 주면 된다.
+		
+		  tv.powerOn(); tv.volumeUp(); tv.volumeDown(); tv.powerOff();
+		 
 		
 		factory.close();
 
